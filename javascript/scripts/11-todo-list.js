d0bd99c5aa2653065d92be74ae3b558f57d52778
Boltcrasher <Boltcrasher@gmail.com>
2023-08-28 02:51:40 -0500

    9:02

---
+++ javascript/scripts/11-todo-list.js
@@ -0,0 +1,9 @@
+const todoList = [];
+
+function addToDo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    todoList.push(name);
+    console.log(todoList);
+    inputElement.value = '';
+}
